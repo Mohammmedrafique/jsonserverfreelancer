476ad1e7e1d7442d5e49330700d564dd532cc89a
Mohammmedrafique <pushpendrasharmakarauli@gmail.com>
2023-11-23 17:04:52 +0530

    s

--- index.js
+++ index.js
@@ -2,10 +2,12 @@
 const jsonServer = require("json-server");
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
+const freelancer = jsonServer.router("freelancers.json");
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
 server.use(router);
+server.use(freelancer);
 
 server.listen(8000, () => {
   console.log("JSON Server is running");
